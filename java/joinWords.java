5574cd298763663425935c04b16bee84ea815099
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-05-30 12:29:45 -0500

    Failed Java Concat

---
+++ java/joinWords.java
@@ -0,0 +1,12 @@
+public class joinWords {
+    String concatSent (String[] words){
+        String concatNew = "";
+        for(String w : words){
+            concatNew = concatNew + w;
+        }
+        
+        return concatNew;
+
+    }
+
+}
