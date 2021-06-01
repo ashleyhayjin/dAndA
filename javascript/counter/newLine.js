25d764bb8866189bcf7778bc55df5b5ead18a9f1
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-01 16:05:04 -0500

    Max Arr Sum Algorithm

--- javascript/counter/newLine.js
+++ javascript/counter/newLine.js
@@ -11,4 +11,33 @@ function processData(input) {
     });
 };
 
-processData(Hello)
+
+function processData2 (input){
+let first = input.split("\n");
+let new1 = first[1].split("");
+let new2 = first[2].split("");
+let third = [];
+let fourth = [];
+let third2 = [];
+let fourth2 = [];
+for(let i =0; i < new1.length; i++){
+    if(i % 2 == 0){
+        third.push(new1[i]);
+    } else {
+        fourth.push(new1[i]);
+    }
+}
+for(let i =0; i < new2.length; i++){
+    if(i % 2 == 0){
+        third2.push(new2[i]);
+    } else {
+        fourth2.push(new2[i]);
+    }
+}
+
+let final1 = third.join("") + " " + fourth.join("");
+let final2 = third2.join("") + " " + fourth2.join("");
+console.log(final1);
+console.log(final2);
+// Code failed with multiple /n in the input parameter
+};
---
+++ javascript/maxSubArr.js
@@ -0,0 +1,17 @@
+function maxSubArr(arr, num){
+    let tempSum =0;
+    let maxSum = 0;
+    if(arr.length < num) return null;
+    for(let i=0; i <num; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    console.log(maxSum);
+    return maxSum;
+}
+
+maxSubArr([1,1,1,1,4], 3);
