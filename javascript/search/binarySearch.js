c6e3a56271e72a5ddd7676e874c628ca90544235
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-14 12:56:21 -0500

    binary

--- javascript/search/binarySearch.js
+++ javascript/search/binarySearch.js
@@ -1,9 +1,23 @@
 function binarySearch(arr, target){
     // add whatever parameters you deem necessary - good luck!
-    let left =0;
-    let right = arr.length-1;
-    let middle = arr.length-1 / 2;
-    
-    // if(arr[middle] <)
-    
-  }
+      let left =0;
+      let right = arr.length-1;
+      
+      while(left < right){
+      
+       if(arr[left] === target){
+          return left;
+      }
+      if(arr[right] === target){
+          return right;
+      }
+      if(arr[left] < target){
+          left++;
+      } else if( arr[right] > target){
+          right--;
+      } 
+      }
+      return -1
+  }    
+
+  
