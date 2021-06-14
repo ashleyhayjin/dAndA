6aaac5958c2464bd0538499844015e2acdd75094
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-14 12:39:12 -0500

    search folder + binary search

---
+++ javascript/search/binarySearch.js
@@ -0,0 +1,9 @@
+function binarySearch(arr, target){
+    // add whatever parameters you deem necessary - good luck!
+    let left =0;
+    let right = arr.length-1;
+    let middle = arr.length-1 / 2;
+    
+    // if(arr[middle] <)
+    
+  }
