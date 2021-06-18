d2264b1d995e6c0f18980da13a0eccbe5925a9c1
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-18 17:28:32 -0500

    class structures for OOP js

---
+++ datastruct/class/classPrac.js
@@ -0,0 +1,10 @@
+class Student {
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+    }
+}
+
+let ashley = new Student ("Ashley", "Lee");
+console.log(ashley);
