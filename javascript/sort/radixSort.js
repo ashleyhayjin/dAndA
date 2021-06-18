0d407c5cea575096020f971986988b70607766ea
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-18 16:55:11 -0500

    helpers and radixSort

--- javascript/sort/radixSort.js
+++ javascript/sort/radixSort.js
@@ -11,16 +11,30 @@ function digitCount(num){
     if(num===0) return 1;
     return Math.floor(Math.log10(Math.abs(num))) +1;
 }
+//  given an arr of numbers return the number of digits in the largest numbers in the list
+function mostDigits(nums){
+    let maxDigits =0;
+    for(let i=0; i<nums.length;i++){
+        maxDigits =Math.max(maxDigits, digitCount(nums[i]))
+    }
+    return maxDigits;
+}
 
 
 
 
+function radixSort(nums){
+    let maxDigitCount = mostDigits(nums);
+    for(let k =0; k <maxDigitCount;k++){
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for(let i=0; i < nums.length; i++){
+            digitBuckets[getDigit(nums[i], k)].push(nums[i]);
+        }
+        console.log(digitBuckets);
+        nums = [].concat(...digitBuckets);
+        console.log(nums);
+    }
+    return nums;
+}
 
-
-
-
-
-
-
-
-function radixSort(){}
+radixSort([1,11,1111,1111])
