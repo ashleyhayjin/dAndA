576d9a72722d280e5964b6a65569a59688657ead
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-18 17:48:44 -0500

    Linked List starter

---
+++ datastruct/linkedList/linkedList.js
@@ -0,0 +1,5 @@
+// A data structure that is ordered. Difference between linked list and array is array has an index and linked list consist of nodes
+// Each node has a value and a pointer.
+// Head is beginning, Tail is the end. You also keep track of the length, not what is in the middle
+// one node points to the next one, a skyscraper with no elevators. It cannot take you to the certain floor, you have to take the stairs 
+// singly linked lists, each node only connects to one node, next to each other.
