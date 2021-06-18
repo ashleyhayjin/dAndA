ef80c1407a63e45b2768081d3dfce2225b5f3b71
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-17 22:27:27 -0500

    swap

---
+++ javascript/sort/quickSort.js
@@ -0,0 +1,27 @@
+function pivot(arr, start=0, end=arr.length){
+    
+    function swap(array,i,j){
+        var temp = arr[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    var pivot = arr[start];
+    var swapIndex = start;
+
+    for(var i= start; i < arr.length; i++){
+        if(pivot > arr[i]){
+            swapIndex++;
+            swap(arr,swapIndex, i)
+        }
+    }
+    swap(arr,start,swapIndex);
+    console.log(arr);
+    return swapIndex;
+}
+
+pivot([4,8,2,1,5,7,6,3])
+// [4,8,2,1,5,7,6,3]
+// [4,2,8,1,5,7,6,3]
+// [4,2,8,1,5,7,6,3]
+// [4,2,1,8,5,7,6,3]
+// [4,2,1,3,5,7,6,8]
