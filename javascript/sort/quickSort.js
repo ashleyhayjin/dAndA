206a00c671c4abaf776f96c471f4418f65171835
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-17 22:38:44 -0500

    finalized quick Sort

--- javascript/sort/quickSort.js
+++ javascript/sort/quickSort.js
@@ -25,3 +25,13 @@ pivot([4,8,2,1,5,7,6,3])
 // [4,2,8,1,5,7,6,3]
 // [4,2,1,8,5,7,6,3]
 // [4,2,1,3,5,7,6,8]
+
+function quickSort(arr, left=0, right=arr.length-1){
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIndex-1);
+        quickSort(arr, pivotIndex+1, right);
+    }
+    return arr;
+}
