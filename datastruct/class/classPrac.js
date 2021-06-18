8a5f55b371f1054ad31d55a68427e7b99f2e818b
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-18 17:37:19 -0500

    instance methods

--- datastruct/class/classPrac.js
+++ datastruct/class/classPrac.js
@@ -3,8 +3,32 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.year = year;
+        this.tardies = 0;
+    }
+    fullName(){
+        return `Your name is ${this.firstName} ${this.lastName}`;
+    }
+    markLate(){
+        this.tardies+=1;
+        if(this.tardies >=3){
+            return `${this.firstName} ${this.lastName} are expelled.`
+        }
+    }
+
+    addScore(score){
+        this.score.push(score);
+        return this.scores
+    }
+
+    calculateAverage(){
+        let sum = this.scores.reduce(function(a,b){return a+b})
+        return sum/this.scores.length;
     }
 }
-
+// single instance methods 
 let ashley = new Student ("Ashley", "Lee");
 console.log(ashley);
+console.log(ashley.fullName());
+
+// constructor methods 
+// instance vs static
