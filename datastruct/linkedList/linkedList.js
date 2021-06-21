64aba49ec47be88654fc86c2518fc416b7d27f0a
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-21 11:57:19 -0500

    New node

--- datastruct/linkedList/linkedList.js
+++ datastruct/linkedList/linkedList.js
@@ -13,3 +13,26 @@ class Node {
 }
 
 var first = new Node("Hi");
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+var list = new SinglyLinkedList()
+list.push("Hi");
