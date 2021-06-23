8aabfeb6351b03634dcbf0e2229dd98d4fd81b06
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-23 16:30:30 -0500

    mountain array algo

---
+++ algo/javascript/search/mountainArr.js
@@ -0,0 +1,16 @@
+var validMountainArray = function(arr) {
+    let i =0;
+ 
+    while(i +1 < arr.length && arr[i] < arr[i+1]){
+        i++;
+    }
+    if(i === 0 || i === arr.length-1){
+            return false;
+    }
+    while(i +1 < arr.length && arr[i] > arr[i+1]){
+        i++;
+    }
+    
+    
+    return i=== arr.length-1
+};
