ffd9ccdbd44b45dde4948ddb0aac54babf3c659c
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-23 20:36:59 -0500

    doubly linked list

---
+++ datastruct/doubleLinked/double.js
@@ -0,0 +1,16 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
