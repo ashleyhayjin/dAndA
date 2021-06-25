e4c14b7f79bcb935a97786e3a12611142673e77f
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-25 15:45:38 -0500

    Queue starter

---
+++ datastruct/stackAndQ/queue.js
@@ -0,0 +1,18 @@
+// add and remove data 
+// FIFO Data structures 
+// First in first out
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+}
