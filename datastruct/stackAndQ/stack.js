a49730697b15e00b51440d924bb41403286163b4
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-25 15:34:42 -0500

    stack push

--- datastruct/stackAndQ/stack.js
+++ datastruct/stackAndQ/stack.js
@@ -1,3 +1,29 @@
 // LIFO
 // Last in first out
 
+class Node {
+    constructor(val){
+        this.value = value;
+        this.next = null;
+    }
+}
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else{
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+}
+
