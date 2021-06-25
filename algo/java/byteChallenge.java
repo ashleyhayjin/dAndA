cd18f357c8652732b3c97b262ec70c3daa620233
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-25 16:37:36 -0500

    all

---
+++ algo/java/byteChallenge.java
@@ -0,0 +1,11 @@
+
+public class byteChallenge {
+    public static void main(String[] args){
+        byte first = 15;
+        short second = 20;
+        int third = 10000;
+
+        long myTotal = 50000 + (10 * (first + second + third));
+        System.out.println(myTotal);
+    }
+}
