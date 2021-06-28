bed30c16a2077b2c9234807c7dd71f89f0e0cef6
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-28 11:09:04 -0500

    BFS

--- datastruct/trees/tree.js
+++ datastruct/trees/tree.js
@@ -51,6 +51,30 @@ class BinarySearchTree{
            } 
         }
     }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            }
+        } 
+    }
+    BFS(){
+        var data = [],
+            queue = [],
+            node = this.root;
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value;
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+        
+    }
 }
 
 
