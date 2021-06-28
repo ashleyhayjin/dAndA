39f9216a1d4cce15ec39b80230b7b149e215301f
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-28 10:28:36 -0500

    insert

--- datastruct/trees/tree.js
+++ datastruct/trees/tree.js
@@ -31,7 +31,24 @@ class BinarySearchTree{
             return this;
         } else {
            var current = this.root;
-            
+           while(true){
+               if(value === current.value) return undefined;
+               if(value < current.value){
+                   if(current.left === null){
+                   current.left = newNode;
+                   return this;
+               } else {
+                   current = current.left;
+               }
+            }else if(value > current.value){
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } else {
+                    current = current.right;
+                }
+            }
+           } 
         }
     }
 }
