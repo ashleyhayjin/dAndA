34cc234f85cbfb165205501bf34b6cfb85433c73
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-06-30 21:52:14 -0500

    squarre bracket

--- datastruct/trees/tree.js
+++ datastruct/trees/tree.js
@@ -68,7 +68,7 @@ class BinarySearchTree{
         queue.push(node);
         while(queue.length){
             node = queue.shift();
-            data.push(node.value;
+            data.push(node.value);
             if(node.left) queue.push(node.left);
             if(node.right) queue.push(node.right);
         }
